bb959d68ab1c9210ebbe2ab8f1578fc7afa8ed94
Case Wylie <cmwylie19@defenseunicorns.com>
2024-03-12 12:37:51 -0400

    chore: ignore-line
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- src/fluent/index.ts
+++ src/fluent/index.ts
@@ -46,7 +46,7 @@ export function K8s<T extends GenericClass, K extends KubernetesObject = Instanc
    */
   function WithField<P extends Paths<K>>(key: P, value: string) {
     filters.fields = filters.fields || {};
-    // @ts-ignore
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     filters.fields[key] = value;
     return withFilters;
   }
