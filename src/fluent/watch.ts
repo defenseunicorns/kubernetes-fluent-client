71b160096ffe52a51bcba1b9f315278946a2df54
Megamind <882485+jeff-mccoy@users.noreply.github.com>
2024-05-17 16:03:55 -0500

    Update src/fluent/watch.ts
    Co-authored-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- src/fluent/watch.ts
+++ src/fluent/watch.ts
@@ -4,7 +4,6 @@
 import byline from "byline";
 import { createHash } from "crypto";
 import { EventEmitter } from "events";
-import { Agent } from "https";
 import fetch from "node-fetch";
 
 import { GenericClass } from "../types";
