24b9207e194caca2bf2ce87fc182bbc5d049bce1
Case Wylie <cmwylie19@defenseunicorns.com>
2024-10-24 12:48:42 -0400

    chore: upstream
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- src/upstream.ts
+++ src/upstream.ts
@@ -49,7 +49,7 @@ export {
   V1TokenReview as TokenReview,
   V1ValidatingWebhookConfiguration as ValidatingWebhookConfiguration,
   V1VolumeAttachment as VolumeAttachment,
-  V1Endpoint as Endpoint
+  V1Endpoint as Endpoint,
 } from "@kubernetes/client-node";
 
 export { GenericKind } from "./types";
