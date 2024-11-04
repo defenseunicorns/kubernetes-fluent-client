74102e6a8b3100a65f19bead83edd29b4b18ba15
Case Wylie <cmwylie19@defenseunicorns.com>
2024-11-04 17:50:39 -0500

    chore: run format
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,47 +9,58 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [{
+export default [
+  {
     ignores: ["**/node_modules", "**/dist", "**/__mocks__", "e2e/crds", "e2e/crds"],
-}, ...compat.extends(
+  },
+  ...compat.extends(
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:jsdoc/recommended-typescript-error",
-), {
+  ),
+  {
     plugins: {
-        "@typescript-eslint": typescriptEslint,
+      "@typescript-eslint": typescriptEslint,
     },
 
     languageOptions: {
-        globals: {
-            ...Object.fromEntries(Object.entries(globals.browser).map(([key]) => [key, "off"])),
-        },
+      globals: {
+        ...Object.fromEntries(Object.entries(globals.browser).map(([key]) => [key, "off"])),
+      },
 
-        parser: tsParser,
-        ecmaVersion: 2022,
-        sourceType: "script",
+      parser: tsParser,
+      ecmaVersion: 2022,
+      sourceType: "script",
 
-        parserOptions: {
-            project: ["tsconfig.json"],
-        },
+      parserOptions: {
+        project: ["tsconfig.json"],
+      },
     },
 
     rules: {
-        "class-methods-use-this": "warn",
-        "consistent-this": "warn",
-        "no-invalid-this": "warn",
+      "class-methods-use-this": "warn",
+      "consistent-this": "warn",
+      "no-invalid-this": "warn",
 
-        "@typescript-eslint/no-floating-promises": ["warn", {
-            ignoreVoid: true,
-        }],
+      "@typescript-eslint/no-floating-promises": [
+        "warn",
+        {
+          ignoreVoid: true,
+        },
+      ],
 
-        "jsdoc/tag-lines": ["error", "any", {
-            startLines: 1,
-        }],
+      "jsdoc/tag-lines": [
+        "error",
+        "any",
+        {
+          startLines: 1,
+        },
+      ],
     },
-}];
+  },
+];
