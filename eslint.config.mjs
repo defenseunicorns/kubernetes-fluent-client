bc4f209b46645f96549fbdf530aaf19c9e4c2cea
Case Wylie <cmwylie19@defenseunicorns.com>
2025-04-24 14:48:12 -0400

    chore: eslint config
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,7 @@ const compat = new FlatCompat({
 
 export default [
   {
-    ignores: ["**/node_modules", "**/dist", "**/__mocks__", "e2e/**"],
+    ignores: ["**/node_modules", "**/dist", "**/__mocks__", "e2e/**",".github/workflows/matrix.js"],
   },
   ...compat.extends(
     "eslint:recommended",
